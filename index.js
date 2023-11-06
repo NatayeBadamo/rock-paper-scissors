4ec25c070aa583927ae23ad5cbd01a7d62367022
lowin <149538744+NatayeBadamo@users.noreply.github.com>
2023-11-06 22:39:25 +0000

    Added new functionalities

--- index.js
+++ index.js
@@ -1,39 +1,87 @@
-function getComputerChoice(){
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
     const strings = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * strings.length);
     return strings[randomIndex];
 }
 
-function game(playerSelection){
-    result = '';
-
+function playRound(playerSelection) {
     const computerChoice = getComputerChoice();
+    let result = '';
 
-    if (playerSelection === 'rock'){
-        if (computerChoice === 'rock'){
+    if (playerSelection === 'rock') {
+        if (computerChoice === 'rock') {
             result = 'The game is tie';
-        }else if (computerChoice === 'paper'){
+        } else if (computerChoice === 'paper') {
             result = 'You lose the game';
-        }else if (computerChoice === 'scissors'){
+            computerScore++;
+        } else if (computerChoice === 'scissors') {
             result = 'You win the game';
+            playerScore++;
         }
-    } else if (playerSelection === 'paper'){
-        if (computerChoice === 'rock'){
+
+    } else if (playerSelection === 'paper') {
+        if (computerChoice === 'rock') {
             result = 'You win the game';
-        }else if (computerChoice === 'paper'){
+            playerScore++;
+        } else if (computerChoice === 'paper') {
             result = 'The game is tie';
-        }else if (computerChoice === 'scissors'){
+        } else if (computerChoice === 'scissors') {
             result = 'You lose the game';
-        } 
-    } else if (playerSelection === 'scissors'){
-        if (computerChoice === 'rock'){
+            computerScore++;
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerChoice === 'rock') {
             result = 'You lose the game';
-        }else if (computerChoice === 'paper'){
+            computerScore++;
+        } else if (computerChoice === 'paper') {
             result = 'You win the game';
-        }else if (computerChoice === 'scissors'){
+            playerScore++;
+        } else if (computerChoice === 'scissors') {
             result = 'The game is tie';
-        } 
+        }
+    
+
     }
 
     document.getElementById('result').textContent = result;
+
+    document.getElementById('playerScore').textContent = `Player Score: ${playerScore}`; 
+    document.getElementById('computerScore').textContent = `Computer Score: ${computerScore}`;
+    
+    function winner() {
+        if (playerScore >= 5) {
+            document.getElementById('winner').textContent = "You are the winner";
+        } else if (computerScore >= 5) {
+            document.getElementById('winner').textContent = "The computer wins over you";
+        } else {
+            document.getElementById('winner').textContent = "Game in progress";
+        }
+    }
+
+    winner();
+    
+
+
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
